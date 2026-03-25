ed1ac0e27635ea682515507b1418af0b0c91254f
TheSillyOk <priv.ld@proton.me>
2026-03-24 23:05:06 -0300

    improve: logger.h to have more logging options
    Signed-off-by: TheSillyOk <priv.ld@proton.me>

--- src/logger.h
+++ src/logger.h
@@ -4,17 +4,22 @@
 #include <stdlib.h>
 
 #include <android/log.h>
+#include <errno.h>
 
 #define TAG "ZygiskModuleExample"
 
 #ifdef DEBUG
   #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+  #define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, TAG, __VA_ARGS__)
 #else
   #define LOGD(...)
+  #define LOGV(...)
 #endif
 
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
-#define LOGF(...) do { __android_log_print(ANDROID_LOG_FATAL, TAG, __VA_ARGS__); abort(); } while (0)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
+#define LOGF(...) __android_log_print(ANDROID_LOG_FATAL, TAG, __VA_ARGS__)
+#define PLOGE(fmt, args...) LOGE(fmt " failed with %d: %s", ##args, errno, strerror(errno))
 
 #endif /* LOGGER_H */
